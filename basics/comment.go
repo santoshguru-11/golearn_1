0fcbc3964495599f9f218f2e7528b3857dec75c8
santoshguru-11 <d.vns.gurusantosh11@gmail.com>
2024-04-09 20:01:36 +0530

    add file comment.go

---
+++ basics/comment.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// this is a single line comment
+// this is a single line comment
+func main() {
+	/*
+	   this is a
+	   multi line
+	   comment
+	*/
+	fmt.Println("Hello World")
+}
