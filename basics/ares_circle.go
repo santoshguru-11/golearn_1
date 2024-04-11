a4749e2503192fc969da83f0180ff1d7303a7621
santoshguru-11 <d.vns.gurusantosh11@gmail.com>
2024-04-11 22:44:34 +0530

    add area_circle.go

---
+++ basics/ares_circle.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const PI float64 = 3.14 // global constant
+func main() {
+	var radius float64 = 5.0
+	var area float64
+	area = PI * radius * radius
+	fmt.Println("Area of Circle is : ", area)
+
+}
