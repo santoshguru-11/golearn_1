dd3cb7d72c561fdb9740bb14cff216e6b65bf95b
santoshguru-11 <d.vns.gurusantosh11@gmail.com>
2024-04-11 22:34:52 +0530

    added flot_int.go

---
+++ basics/flot_int.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var f float64 = 45.89
+	var i int = int(f)
+	fmt.Printf("%v\n", i)
+}
---
+++ basics/in_flot.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var i int = 90
+	var f float64 = float64(i)
+	fmt.Printf("%.2f\n", f)
+
+}
