f02947d24688e4780ca8c5132be3f658d7125e07
santoshguru-11 <d.vns.gurusantosh11@gmail.com>
2024-04-11 22:40:38 +0530

    ADDED constant.go

---
+++ basics/constant.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const name = "Harry Potter"
+	const is_muggle = false
+	const age = 12
+	fmt.Printf("%v: %T \n", name, name)
+	fmt.Printf("%v: %T \n", is_muggle, is_muggle)
+	fmt.Printf("%v: %T", age, age)
+
+}
