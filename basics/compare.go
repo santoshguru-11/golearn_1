0220df25f16f69b71603bbed64767eb26ef20699
santoshguru-11 <d.vns.gurusantosh11@gmail.com>
2024-04-12 21:50:36 +0530

    added comare.go

---
+++ basics/compare.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declaring variables
+	var a, b int
+	a = 10
+	b = 20
+
+	// Comparison operators
+	fmt.Println("Comparison Operators:")
+	fmt.Println("a == b:", a == b) // Equal to
+	fmt.Println("a != b:", a != b) // Not equal to
+	fmt.Println("a < b:", a < b)   // Less than
+	fmt.Println("a <= b:", a <= b) // Less than or equal to
+	fmt.Println("a > b:", a > b)   // Greater than
+	fmt.Println("a >= b:", a >= b) // Greater than or equal to
+}
