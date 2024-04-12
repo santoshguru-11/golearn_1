3efa1eacc42c16a7eeb480fd2a5fd5af26a32bb3
santoshguru-11 <d.vns.gurusantosh11@gmail.com>
2024-04-12 21:52:32 +0530

    added arthemathic.go

---
+++ basics/arthematic.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declaring variables
+	var a, b int
+	a = 10
+	b = 5
+
+	// Arithmetic operators
+	fmt.Println("Arithmetic Operators:")
+	fmt.Println("a + b:", a+b) // Addition
+	fmt.Println("a - b:", a-b) // Subtraction
+	fmt.Println("a * b:", a*b) // Multiplication
+	fmt.Println("a / b:", a/b) // Division
+	fmt.Println("a % b:", a%b) // Modulus (remainder of division)
+}
