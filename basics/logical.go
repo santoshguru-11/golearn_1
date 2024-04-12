9006b9125c97eb822220594cd2ec4e7840a4e546
santoshguru-11 <d.vns.gurusantosh11@gmail.com>
2024-04-12 21:54:07 +0530

    added logical.go

---
+++ basics/logical.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declaring variables
+	var x, y bool
+	x = true
+	y = false
+
+	// Logical operators
+	fmt.Println("Logical Operators:")
+	fmt.Println("x && y:", x && y) // Logical AND
+	fmt.Println("x || y:", x || y) // Logical OR
+	fmt.Println("!x:", !x)         // Logical NOT
+}
