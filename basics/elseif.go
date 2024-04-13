d24c8c77a0fea389ca81301b141a4a09e6ae602a
santoshguru-11 <d.vns.gurusantosh11@gmail.com>
2024-04-13 09:06:45 +0530

    added elseif.go

---
+++ basics/elseif.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fruit := "grapes"
+
+	if fruit == "apple" {
+		fmt.Println("I love apples")
+	} else if fruit == "orange" {
+		fmt.Println("Oranges are not apples")
+	} else {
+		fmt.Println("no appetite")
+	}
+}
