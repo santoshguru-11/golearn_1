0c2f2be448dba58ca4d48620b001fdcc9e7c9e15
santoshguru-11 <d.vns.gurusantosh11@gmail.com>
2024-04-13 09:05:12 +0530

    added if_else.go

---
+++ basics/if_else.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var fruit string = "grapes"
+	var fruit_1 string = "apples"
+	if fruit == "apples" {
+		fmt.Println("Fruit is apple")
+	} else {
+		fmt.Println("Fruit is not apple")
+	}
+
+	if fruit_1 == "apples" {
+		fmt.Println("Fruit_1 is apple")
+	} else {
+		fmt.Println("Fruit_1 is not apple")
+	}
+
+}
